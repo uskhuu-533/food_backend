1d618fc1b1680f1c10eb1d8e2173919e43edbc2a
uskhuu-533 <uskhuunymdavaa9@gmail.com>
2025-03-06 19:58:00 +0800

    1

--- src/controller/foodOrderItems/get-Item-id.js
+++ src/controller/foodOrderItems/get-Item-id.js
@@ -4,7 +4,7 @@ export const getOneItem = async (req, res) =>{
     const { id } = req.params
     try{
         const item = await FoodOrderItems.findById({_id:id})
-        console.log(item);
+        console.log(item)
         
         res.send(item).status(200)
     }catch(error){
