cc88a385af98e507a2fc706cb54ec882c28b0a44
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-03-11 17:31:46 +0800

    10

--- src/index.js
+++ src/index.js
@@ -12,18 +12,27 @@ import connectDB from "./connectDB.js";
 const app = express();
 const port = 3000;
 connectDB();
-
 app.use(
   cors({
-    origins: [
-      "https://food-delivery-user-lemon.vercel.app",
-      " http://localhost:3001",
-      " http://localhost:3002",
-    ],
-    methods : ["DELETE", "PUT", "GET", "POST", "PATCH"],
-    allowedHeaders: ['Content-Type', 'Authorization'] 
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "https://food-delivery-user-lemon.vercel.app",
+        "http://localhost:3001",
+        "http://localhost:3002",
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["DELETE", "PUT", "GET", "POST", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+
+
 //path => GET, POST, PUT, DELETE
 app.use(express.json());
 app.use(`/users`, userRouter);
