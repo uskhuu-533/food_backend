a97c44014d52d5d1c8e586c8ddfcff5da70df66c
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-03-11 15:38:06 +0800

    z

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
-import express from "express"
-import cors from "cors"
-import { userRouter} from "./routers/user-router.js";
+import express from "express";
+import cors from "cors";
+import { userRouter } from "./routers/user-router.js";
 import mongoose from "mongoose";
 import { categoryRouter } from "./routers/category-router.js";
 import { foodRouter } from "./routers/food-router.js";
-import 'dotenv/config'
-import {  orderItemsRouter } from "./routers/foodOrderItems.router.js";
+import "dotenv/config";
+import { orderItemsRouter } from "./routers/foodOrderItems.router.js";
 import { orderRouter } from "./routers/order.router.js";
 import connectDB from "./connectDB.js";
 
-
 const app = express();
 const port = 3000;
-connectDB()
+connectDB();
 
-app.use(cors({origins:["https://food-delivery-user-lemon.vercel.app/login", " http://localhost:3001", " http://localhost:3002"]}));
+app.use(
+  cors({
+    origins: [
+      "https://food-delivery-user-lemon.vercel.app/login",
+      " http://localhost:3001",
+      " http://localhost:3002",
+    ],
+    methods : ["DELETE", "PUT", "GET", "POST", "PATCH"],
+    allowedHeaders: ['Content-Type', 'Authorization'] 
+  })
+);
 //path => GET, POST, PUT, DELETE
-app.use(express.json()); 
+app.use(express.json());
 app.use(`/users`, userRouter);
-app.use("/category", categoryRouter)
-app.use('/food', foodRouter)
-app.use('/foodorderitems', orderItemsRouter)
-app.use('/foodorder', orderRouter)
+app.use("/category", categoryRouter);
+app.use("/food", foodRouter);
+app.use("/foodorderitems", orderItemsRouter);
+app.use("/foodorder", orderRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });
-
