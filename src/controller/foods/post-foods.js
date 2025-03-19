63de473319e5039a349f635a0420a2e53bb67c40
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-03-19 18:49:12 +0800

    mmm

--- src/controller/foods/post-foods.js
+++ src/controller/foods/post-foods.js
@@ -1,15 +1,19 @@
 import { Foods } from "../../models/foods.model.js";
 
-
 export const postFood = async (req, res) => {
-    const {food_description, food_name, price, food_image} = req.body;
-    const { category } = req.params
-    try {
-
-        const newfood = new Foods({food_description, food_name, price, food_image, category})
-         await newfood.save();  
-        res.status(200).send(`food added: ${newfood}`)
-    } catch (error) {
-        console.log(error);
-    }
-}
+  const { food_description, food_name, price, food_image } = req.body;
+  const { category } = req.params;
+  try {
+    const newfood = new Foods({
+      food_description,
+      food_name,
+      price,
+      food_image,
+      category,
+    });
+    await newfood.save();
+    res.status(200).send(`food added`);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/middleware/food/isdetailvalid.js
+++ src/middleware/food/isdetailvalid.js
@@ -2,6 +2,8 @@ export const isfoodValid = async (req, res, next) => {
   const { food_description, food_name, price, food_image } = req.body;
   if (food_description && food_name && price && food_image) {
     next();
+  }else{
+    res.send("required").status(500)
   }
-  res.send("required").status(500)
+ 
 };
