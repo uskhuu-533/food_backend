f4c542b821120e346a92ac91b7dd9b399467dbb7
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-03-21 18:31:05 +0800

    a

--- src/controller/foods/get-foods.js
+++ src/controller/foods/get-foods.js
@@ -4,7 +4,7 @@ export const getFoods = async (req, res) => {
     const {category} = req.params    
   try {
     if(category){
-      const foods  = await Foods.find({category:category}).populate('category').exec()
+      const foods  = await Foods.find({category:category})
       res.status(200).send(foods)
     }else{
       res.status(500).send("category not found getFoods")
