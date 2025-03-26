5c2404529c67dab7e1eea250d157483ec7438838
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2025-03-26 15:35:54 +0800

    m

--- src/controller/users/get-user.js
+++ src/controller/users/get-user.js
@@ -5,7 +5,7 @@ export const getUser = async (req, res) => {
   const _id = req.userId
   try {
     const usersFromDB = await User.findById({_id});
-    res.status(200).send(usersFromDB.email);
+    res.status(200).send({email : usersFromDB.email, address : usersFromDB.address, role:usersFromDB.role});
   } catch (err) {
     console.log(err);
   }
